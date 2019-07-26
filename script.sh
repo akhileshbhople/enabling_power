b089bfbe92b38f4ab5eab7ac1921c8475d6a87d2
Akhilesh Bhople <abhople@us.ibm.com>
2019-07-26 09:06:24 -0400

    fix file permissions

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == linux ]]
+if [ $TRAVIS_OS_NAME == 'linux' ]
 then
 	cat /etc/os-release
-elif [[ "$TRAVIS_OS_NAME" == osx ]]
+elif [ $TRAVIS_OS_NAME == 'osx' ]
 then
 	system_profiler SPSoftwareDataType
 fi
